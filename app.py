048daa146b1c2fbf4abfe373b5c5d2eff396db76
Egezon Baruti <63745689+xoniks@users.noreply.github.com>
2024-11-27 07:06:35 +0000

    cv updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-css_file = current_dir / "styles" / "main.css"
+# css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "CV.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.png"
 
